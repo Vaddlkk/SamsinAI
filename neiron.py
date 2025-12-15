d00f5bdf0e31acc1453ed68b2ffad25dad25af82
OriGiu <somerare22@gmail.com>
2025-12-15 16:19:04 +0300

    Update neiron.py

--- neiron.py
+++ neiron.py
@@ -1,23 +1,33 @@
 import random
+from modules import ints
 
-def ints(numbers):
-    adstring = "".join(map(str, numbers))
-    digitsl = list(adstring)
-    random.shuffle(digitsl)
-    sstring = "".join(digitsl)
-    number = int(sstring)
-    return number
-
-def neiron(name):
-    lists = [random.randint(1, 100) for _ in range(1000)]
+def neiron(name=None):
+    lists = [random.randint(1, 100) for _ in range(50)]
     item = 1
     items = len(lists)
-    count = random.randint(item, 500)
+    count = random.randint(item, items)
     result = random.sample(lists, count)
     result2 = ints(result)
     result3 = bin(result2)
     result3 = result3[2:]
-    print(name)
-    print(result3)
+    if not name == None:
+        print(name)
+        print(result3)
+
     return result3
     #print(f"1: {result}\n2:{result2}\n3:{result3}\n4:{result4}")
+
+def neifon(name=None):
+    lists = [random.randint(1, 50) for _ in range(100)]
+    item = 1
+    items = len(lists)
+    count = random.randint(item, items)
+    result = random.sample(lists, count)
+    result2 = ints(result)
+    result3 = bin(result2)
+    result3 = result3[2:]
+    if not name == None:
+        print(name)
+        print(result3)
+
+    return result3
